bdc96ed58adc2d4fdfcb446bce4fae646f2facda
navid1111 <navidkamal568@gmail.com>
2025-06-04 04:43:19 +0600

    feat(auth): add error handling middleware and update signup route for validation errors

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -4,15 +4,20 @@ import { currentuserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/signin";
 import { signUpRouter } from "./routes/signup";
 import { signOutRouter } from "./routes/signout";
+import { errorHandler } from "./middlewares/error-handler";
 
 
 const app = express();
 app.use(json());
-
+app.get('/',(req ,res)=>{
+    res.send("hello")
+    
+})
 app.use(currentuserRouter);
 app.use(signUpRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
+app.use(errorHandler);
 
 
 
--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -14,10 +14,12 @@ router.post('/api/users/signUp',[
 ],(req:Request,res:Response)=>{
     const errors= validationResult(req);
     if(!errors.isEmpty()){
-         res.status(400).send(errors.array());
+         throw new Error("Invalid email or password")
     }
     const{email,password}=req.body;
     console.log("Creating a user ....")
+    throw new Error("Error connecting to the database")
+
     res.send({})
 
     
