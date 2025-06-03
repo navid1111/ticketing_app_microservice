c31ad6b09109fc9abcd65582076706fd2a258bff
navid1111 <navidkamal568@gmail.com>
2025-06-04 02:56:21 +0600

    feat(auth): add express-validator for user signup validation

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,8 +1,28 @@
-import express from 'express'
+import express, {Request,Response} from 'express'
+import { body ,validationResult} from 'express-validator';
+
 
 const router = express.Router()
-router.get('/api/users/signUp',(req,res)=>{
-    res.send("HELLO THERE")
+router.post('/api/users/signUp',[
+    body('email')
+      .isEmail()
+      .withMessage("Email must be valid"),
+    body('password')
+      .trim()
+      .isLength({min:4,max:20})
+      .withMessage("Password must be provided and must be between 4 to 20 characters")
+],(req:Request,res:Response)=>{
+    const errors= validationResult(req);
+    if(!errors.isEmpty()){
+         res.status(400).send(errors.array());
+    }
+    const{email,password}=req.body;
+    console.log("Creating a user ....")
+    res.send({})
+
+    
+
+
 
 })
 
