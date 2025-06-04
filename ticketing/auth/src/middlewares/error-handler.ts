e452ae84baaa6d14ecbcc8bcb0513d0a66eed801
navid1111 <navidkamal568@gmail.com>
2025-06-04 18:10:49 +0600

    feat(auth): implement custom error classes for request validation and database connection errors

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -1,12 +1,20 @@
 import { NextFunction, Request, Response } from "express";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
-export const errorHandler = (
+export const  errorHandler = (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-    console.log("SomeThing Went Wrong",err)
+    if(err instanceof RequestValidationError){
+      console.log("handling this error as request validation error")
+    }
+
+    if(err instanceof DatabaseConnectionError){
+      console.log("handling this error as Database connection error")
+    }
 
    res.status(400).send({
     message:err.message
