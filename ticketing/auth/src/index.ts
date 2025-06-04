37259281ebf5433938c48086a7ba8e01e4062451
navid1111 <navidkamal568@gmail.com>
2025-06-04 19:07:29 +0600

    refactor(auth): improve error handler structure and ensure proper response formatting

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -6,20 +6,21 @@ import { signUpRouter } from "./routes/signup";
 import { signOutRouter } from "./routes/signout";
 import { errorHandler } from "./middlewares/error-handler";
 
-
 const app = express();
 app.use(json());
+
 app.get('/',(req ,res)=>{
     res.send("hello")
-    
 })
+
+// Register all routes first
 app.use(currentuserRouter);
 app.use(signUpRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
+
+// Error handler must be registered LAST
 app.use(errorHandler);
-
-
 
 app.listen(3000,()=>{
     console.log("listening to port 3000  !!!")
--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -2,21 +2,39 @@ import { NextFunction, Request, Response } from "express";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { DatabaseConnectionError } from "../errors/database-connection-error";
 
-export const  errorHandler = (
+export const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
-) => {
-    if(err instanceof RequestValidationError){
-      console.log("handling this error as request validation error")
-    }
+): void => {
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.errors.map(error => {
+      return {
+        message: error.msg,
+        field: error
+      };
+    });
+    res.status(400).send({ errors: formattedErrors });
+    return;
+  }
 
-    if(err instanceof DatabaseConnectionError){
-      console.log("handling this error as Database connection error")
-    }
+  if (err instanceof DatabaseConnectionError) {
+    res.status(500).send({
+      errors: [
+        {
+          message: err.reason
+        }
+      ]
+    });
+    return;
+  }
 
-   res.status(400).send({
-    message:err.message
-   })
+  res.status(400).send({
+    errors: [
+      {
+        message: err.message
+    }
+    ]
+  });
 };
