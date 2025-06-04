56dfb92032bdd5e45b7d2eeadfca8dd50d05b283
navid1111 <navidkamal568@gmail.com>
2025-06-05 01:29:08 +0600

    refactor(auth): update error handler to use customError for improved error handling

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { customError } from "../errors/custom-error";
 
 export const errorHandler = (
   err: Error,
@@ -8,15 +9,12 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ): void => {
-  if (err instanceof RequestValidationError) {
+  if (err instanceof customError) {
     res.status(err.statusCode).send({ errors: err.serializeError() });
     return;
   }
 
-  if (err instanceof DatabaseConnectionError) {
-    res.status(err.statusCode).send({ errors: err.serializeError() });
-    return;
-  }
+  
 
   res.status(400).send({
     errors: [
