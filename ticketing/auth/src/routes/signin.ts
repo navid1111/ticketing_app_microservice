64ffada1f8753aa45f9aaa00616e991dbc1a52d1
navid1111 <navidkamal568@gmail.com>
2025-06-10 10:08:00 +0600

    feat(auth): implement signIn route with email and password validation

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -1,8 +1,22 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { body, validationResult } from 'express-validator'
+import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = express.Router()
-router.get('/api/users/signIn',(req,res)=>{
-    res.send("HELLO THERE")
+router.post('/api/users/signIn',[
+    body('email')
+       .isEmail()
+       .withMessage('Email is required'),
+    body('password')
+       .trim()
+       .notEmpty()
+       .withMessage("Must provide a password")
+],(req:Request,res:Response)=>{
+    const errors= validationResult(req);
+    if(!errors.isEmpty()){
+        throw new RequestValidationError(errors.array())
+    }
+    
 
 })
 
