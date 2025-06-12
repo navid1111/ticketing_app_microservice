8982c158807e24f703d2012c2791374d5a0d61cf
navid1111 <navidkamal568@gmail.com>
2025-06-12 18:11:42 +0600

    feat(auth): refactor currentUser middleware and update currentuser route to use middleware

--- ticketing/auth/src/middlewares/current-user.ts
+++ ticketing/auth/src/middlewares/current-user.ts
@@ -1,13 +1,26 @@
 import { Request,Response,NextFunction } from "express";
 import jwt from 'jsonwebtoken'
+import { ExitStatus } from "typescript";
 
+interface UserPlayload{
+    id:string,
+    email:string
+}
+
+declare global {
+    namespace Express{
+        interface Request{
+            currentUser?:UserPlayload;
+        }
+    }
+}
 export const currentUser=(req:Request,res:Response,next:NextFunction)=>{
     if(!req.session?.jwt){
         return next();
 
     }
     try {
-        const payload=jwt.verify(req.session.jwt,process.env.JWT_KEY!)
+        const payload=jwt.verify(req.session.jwt,process.env.JWT_KEY!) as UserPlayload
         req.currentUser=payload;
         
     } catch (error) {
--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,19 +1,11 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
-router.get("/api/users/currentuser", (req, res) => {
-  try {
-    if (!req.session || !req.session.jwt) {
-      res.send({ currentUser: null });
-      return;
-    }
-
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({currentUser:payload})
-  } catch (error) {
-    res.send({currentUser:null})
-  }
+router.get("/api/users/currentuser",currentUser, (req, res) => {
+  res.send({currentUser:req.currentUser || null})
+  
 });
 
 export { router as currentuserRouter };
