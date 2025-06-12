e1d0a04854b7b972eeb5803089fdebac071af0ee
navid1111 <navidkamal568@gmail.com>
2025-06-12 19:13:31 +0600

    Add package.json for ticketing with development dependencies for testing

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,36 +1,5 @@
-import express from "express";
-import { json } from "body-parser";
-import { currentuserRouter } from "./routes/current-user";
-import "express-async-errors";
-import { signInRouter } from "./routes/signin";
-import { signUpRouter } from "./routes/signup";
-import { signOutRouter } from "./routes/signout";
-import { errorHandler } from "./middlewares/error-handler";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-const app = express();
-app.set("trust proxy", true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.get("/", (req, res) => {
-  res.send("hello");
-});
-
-// Register all routes first
-app.use(currentuserRouter);
-app.use(signUpRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-
-// Error handler must be registered LAST
-app.use(errorHandler);
-
+import { app } from "./app";
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("Jwt not found");
