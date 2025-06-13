5f49f614b0961f0a0eece70fe774d86313365ae5
navid1111 <navidkamal568@gmail.com>
2025-06-13 14:59:02 +0600

    test: add signup test and improve test setup for MongoDB

--- ticketing/auth/src/test/setup.ts
+++ ticketing/auth/src/test/setup.ts
@@ -1,29 +1,33 @@
 import {MongoMemoryServer} from 'mongodb-memory-server'
 import mongoose from 'mongoose'
 import { app } from '../app'
-let mongo:any;
-beforeAll(async()=>{
-     mongo = new MongoMemoryServer();
 
-    const mongoUri=await mongo.getUri();
-    await mongoose.connect(mongoUri,{
+let mongo: MongoMemoryServer;
 
-    })
+beforeAll(async () => {
+    process.env.JWT_KEY = 'asdfasdf';
+    
+    mongo = await MongoMemoryServer.create();
+    const mongoUri = mongo.getUri();
+    
+    await mongoose.connect(mongoUri);
+});
 
-})
-
-beforeEach(async()=>{
+beforeEach(async () => {
     const db = mongoose.connection.db;
     if (!db) {
         throw new Error('Database connection is not established.');
     }
     const collections = await db.collections();
-    for(let collection of collections){
-
+    
+    for (let collection of collections) {
+        await collection.deleteMany({});
     }
-})
+});
 
-afterAll(async()=>{
-    await mongo.stop();
+afterAll(async () => {
+    if (mongo) {
+        await mongo.stop();
+    }
     await mongoose.connection.close();
-})
+});
