5b4a928051c87b382e65285fcc366a12c09d08dd
navid1111 <navidkamal568@gmail.com>
2025-06-22 20:28:34 +0600

    feat(tickets): enhance ticket creation route with validation for title and price

--- ticketing/tickets/src/routes/__test__/new.test.ts
+++ ticketing/tickets/src/routes/__test__/new.test.ts
@@ -1,38 +1,58 @@
-import request from 'supertest'
-import { app } from '../../app'
+import request from "supertest";
+import { app } from "../../app";
 
-it('has a route handler listening to /api/tickets for post request', async () => {
-    const response = await request(app)
-       .post('/api/tickets')
-       .send({});
-    expect(response.status).not.toEqual(404)
-    
-    
-})
-it('It can only be accessed if the user is signed in',async()=>{
-    const response = await request(app)
-       .post('/api/tickets')
-       .send({});
-    expect(response.status).toEqual(401)
-    
-	
-})
-it('returns a status other than 401 if the user is signed in',async()=>{
-    const response = await request(app)
-       .post('/api/tickets')
-       .set('Cookie',global.signin())
-       .send({});
-       console.log(response.body)
-    expect(response.status).toEqual(200)
-    
-	
-})
-it('returns an error if an invalid title is provided',()=>{
-	
-})
-it('creates a ticket with invalid parameters',()=>{
-	
-})
-it('has a route handler listening to /api/tickets for post request',()=>{
-	
-})
+it("has a route handler listening to /api/tickets for post request", async () => {
+  const response = await request(app).post("/api/tickets").send({});
+  expect(response.status).not.toEqual(404);
+});
+it("It can only be accessed if the user is signed in", async () => {
+  const response = await request(app).post("/api/tickets").send({});
+  expect(response.status).toEqual(401);
+});
+it("returns a status other than 401 if the user is signed in", async () => {
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({});
+  console.log(response.body);
+  expect(response.status).not.toEqual(401);
+});
+it("returns an error if an invalid title is provided", async () => {
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "",
+      price: 10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      price: 10,
+    })
+    .expect(400);
+});
+it("creates a ticket with invalid price parameters", async() => {
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "hello",
+      
+    })
+    .expect(400);
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "hello",
+      price: -10,
+    })
+    .expect(400);
+
+});
+it("has a route handler listening to /api/tickets for post request", () => {});
--- ticketing/tickets/src/routes/new.ts
+++ ticketing/tickets/src/routes/new.ts
@@ -1,13 +1,18 @@
-import { currentUser, requireAuth } from '@nksticketss/common'
+import { currentUser, requireAuth, validateRequest } from '@nksticketss/common'
+import { body } from 'express-validator'
 import express, { Request, Response } from 'express'
-const router= express.Router()
+const router = express.Router()
 
-router.post('/api/tickets',currentUser,(req:Request,res:Response,next)=>{
-    console.log('Current user:', req.currentUser);
-    console.log('Cookies:', req.session);
-    next();
-},requireAuth,(req:Request,res:Response)=>{
+router.post('/api/tickets', currentUser, requireAuth, [
+    body('title')
+        .not()
+        .isEmpty()
+        .withMessage("Title is required"),
+    body('price')
+        .isFloat({ gt: 0 })
+        .withMessage("Price must be greater than 0")
+], validateRequest, (req: Request, res: Response) => {
     res.sendStatus(200);
 })
 
-export {router as createTicketRouter}
+export { router as createTicketRouter }
